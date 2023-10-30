0c50319746b70b5436079a10935a6ed5980749e8
sisira <sisira@d7networks.com>
2023-10-30 18:07:09 +0530

    lookup

--- direct7/number_lookup.js
+++ direct7/number_lookup.js
@@ -6,7 +6,7 @@ class NUMBER_LOOKUP {
   async searchNumberDetails(recipient) {
     try {
       const response = await this.client.post('/hlr/v1/lookup', {
-        recipient
+        recipient: recipient
       });
       
       console.log('Search request is success.');
