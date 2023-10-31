c6c2dfd07b13d0c1160c94c56f518b38ef7d485e
sisira <sisira@d7networks.com>
2023-10-31 09:55:10 +0530

    verify

--- direct7/verify.js
+++ direct7/verify.js
@@ -1,68 +1,63 @@
+// const { v4: uuidv4 } = require('uuid');
+
 class VERIFY {
   constructor(client) {
     this.client = client;
   }
 
-  async sendOtp(originator, recipient, content = null, data_coding = null, expiry = null, template_id = null) {
-    try {
-      let response;
-      if (template_id) {
-        response = await this.client.post('/verify/v1/otp/send-otp', {
+  async sendOTP({
+    originator,
+    recipient,
+    content,
+    data_coding,
+    expiry,
+    template_id,
+  }) {
+    const params = template_id
+      ? {
           originator,
           recipient,
-          template_id
-        });
-      } else {
-        response = await this.client.post('/verify/v1/otp/send-otp', {
+          template_id,
+        }
+      : {
           originator,
           recipient,
           content,
           expiry,
-          data_coding
-        });
-      }
-      
-      console.log('OTP Message sent successfully.');
-      return response;
-    } catch (error) {
-      console.log(`Failed to resend OTP: ${error}`);
-      throw error;
-    }
+          data_coding,
+        };
+
+    return this._sendRequest('/verify/v1/otp/send-otp', params, 'OTP Message sent successfully.');
   }
 
-  async resendOtp(otp_id) {
-    const params = { otp_id };
+  async resendOTP(otp_id) {
+    const params = {
+      otp_id,
+    };
 
-    try {
-      const response = await this.client.post('/verify/v1/otp/resend-otp', {params});
-      console.log('OTP Message re-sent successfully.');
-      return response;
-    } catch (error) {
-      console.log(`Failed to resend OTP: ${error}`);
-      throw error;
-    }
+    return this._sendRequest('/verify/v1/otp/resend-otp', params, 'OTP Message Re-sent successfully.');
   }
 
-  async verifyOtp(otp_id, otp_code) {
-    const params = { otp_id, otp_code };
+  async verifyOTP(otp_id, otp_code) {
+    const params = {
+      otp_id,
+      otp_code,
+    };
 
-    try {
-      const response = await this.client.post('/verify/v1/otp/verify-otp', {params} );
-      console.log('OTP Message verified successfully.');
-      return response;
-    } catch (error) {
-      console.log(`Failed to verify OTP: ${error}`);
-      throw error;
-    }
+    return this._sendRequest('/verify/v1/otp/verify-otp', params, 'OTP Message verified successfully.');
   }
 
   async getStatus(otp_id) {
+    return this._sendRequest(`/verify/v1/report/${otp_id}`, null, 'OTP Message status retrieved successfully.');
+  }
+
+  async _sendRequest(endpoint, params, successMessage) {
     try {
-      const response = await this.client.get(`/verify/v1/report/${otp_id}`);
-      console.log('OTP Message status retrieved successfully.');
+      const response = await this.client.post(endpoint, params);
+      console.log(successMessage);
       return response;
     } catch (error) {
-      console.log(`Failed to retrieve OTP status: ${error}`);
+      console.log(`Error: ${error}`);
       throw error;
     }
   }
